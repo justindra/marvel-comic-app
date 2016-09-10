05cb20359387c9db7ec134319db83f4811b2aec8
Justin Rahardjo <justindra91@gmail.com>
2016-09-10 13:15:45 +1000

    Use redux to request comics

--- src/redux-store.js
+++ src/redux-store.js
@@ -1,12 +1,52 @@
 function  buildStore() {
     "use strict";
 
-    function data (state, action) {
-        return action.data || '';
+    var API_KEY = 'bb7b0d40bc2bdfbb870e9955e97c381a';
+
+    var REQUEST_COMICS = 'REQUEST_COMICS';
+    var RECEIVE_COMICS = 'RECEIVE_COMICS';
+
+    function requestComics () {
+        return {
+            type: REQUEST_COMICS
+        };
+    }
+
+    function receiveComics (json) {
+        return {
+            type: RECEIVE_COMICS,
+            data: json.data.results
+        };
+    }
+
+    function fetchNewComics() {
+      return function (dispatch) {
+        dispatch(requestComics());
+        return fetch(`http://gateway.marvel.com:80/v1/public/comics?dateDescriptor=thisMonth&apikey=` + API_KEY)
+          .then(function (response) { return response.json(); })
+          .then(function (json) { return dispatch(receiveComics(json)); });
+      };
+    }
+
+    function curComics (state, action) {
+        switch (action.type) {
+            case REQUEST_COMICS:
+                return Object.assign({}, state, {
+                    loading: true
+                });
+            case RECEIVE_COMICS:
+                return Object.assign({}, state, {
+                    loading: false,
+                    items: action.data
+
+                });
+            default:
+                return state || { loading: false, items: [] };
+        }
     }
 
     var rootReducer = Redux.combineReducers({
-        data: data
+        curComics: curComics
     });
 
     var middleware = Redux.applyMiddleware(ReduxThunk.default);
@@ -16,6 +56,12 @@ function  buildStore() {
             middleware, window.devToolsExtension && window.devToolsExtension() || function (a) { return a; }
         )
     );
+
+    MarvelStore.actions = {};
+
+    MarvelStore.actions.getNewComics = function () {
+        MarvelStore.dispatch(fetchNewComics());
+    }
 
     return MarvelStore;
 }
